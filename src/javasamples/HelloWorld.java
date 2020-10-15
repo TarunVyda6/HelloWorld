78b08b438d06a5d282d14146bf1d832eb8bfcaf3
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-15 09:30:33 +0530

    Update HelloWorld.java
    changes made in println statement of remote repo

--- src/javasamples/HelloWorld.java
+++ src/javasamples/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World Welcome To The Java World");
+		System.out.println("Hello World Welcome To Java World in remote repo");
 	}
 }
